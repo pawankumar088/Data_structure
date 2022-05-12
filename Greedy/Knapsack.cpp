cd9c0c09b90a725de0b15caf63ea5d75f3896752
Pawan <pawankumar08892@gmail.com>
2022-05-12 22:24:22 +0530

    adding more

--- Greedy/Knapsack.cpp
+++ Greedy/Knapsack.cpp
@@ -7,6 +7,12 @@ using namespace std;
 #define ff first
 #define ss second
 #define setBits(x) builtin_popcount(x)
+bool copmare(pii p1, pii p2)
+{
+    double v1 = (double)p1.ff / p1.ss;
+    double v2 = (double)p2.ff / p2.ss;
+    return v1 > v2;
+}
 signed main()
 {
     int n;
@@ -14,13 +20,26 @@ signed main()
     vii a(n);
     rep(i, 0, n)
     {
-        cin >> a[i].ff>>a[i].ss;
+        cin >> a[i].ff >> a[i].ss;
     }
+    int w;
+    cin >> w;
+
     sort(a.begin(), a.end(), copmare);
     int ans = 0;
-    r
+    rep(i, 0, n)
+    {
+        if (w >= a[i].ss)
+        {
+            ans += a[i].ff;
+            w -= a[i].ss;
+            continue;
+        }
+        double vw = (double)a[i].ff / a[i].ss;
+        ans += vw * w;
+        w = 0;
+        break;
+    }
+    cout << ans;
+    return 0;
 }
-
-// 10
-// 1 2 5 10 20 50 100 200 500 200
-// 54
