7d0463305622894d76f9271a774e1dbf6ef25bbe
Pawan <pawankumar08892@gmail.com>
2022-05-12 22:23:36 +0530

    adding more

--- Sorting/Selction_sort.c
+++ Sorting/Selction_sort.c
@@ -1,88 +1,51 @@
+
+
 #include <stdio.h>
-
-void printArray(int *A, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", A[i]);
-    }
-    printf("\n");
-}
-
-void selectionSort(int *A, int n)
-{
-    int indexOfMin, temp;
-    printf("Running Selection sort...\n");
-    for (int i = 0; i < n - 1; i++)
-    {
-        indexOfMin = i;
-        for (int j = i + 1; j < n; j++)
-        {
-            if (A[j] < A[indexOfMin])
-            {
-                indexOfMin = j;
-            }
-        }
-        // Swap A[i] and A[indexOfMin]
-        temp = A[i];
-        A[i] = A[indexOfMin];
-        A[indexOfMin] = temp;
-    }
-}
-
 int main()
 {
-    // Input Array (There will be total n-1 passes. 5-1 = 4 in this case!)
-    //  00  01  02  03  04
-    // |03, 05, 02, 13, 12
-
-    // After first pass
-    //  00  01  02  03  04
-    //  02,|05, 03, 13, 12
-
-    // After second pass
-    // 00  01  02  03  04
-    // 02, 03,|05, 13, 12
-
-    // After third pass
-    // 00  01  02  03  04
-    // 02, 03, 05,|13, 12
-
-    // After fourth pass
-    // 00  01  02  03  04
-    // 02, 03, 05, 12,|13
-
-    int A[] = {3, 5, 2, 13, 12};
-    int n = 5;
-    printArray(A, n);
-    selectionSort(A, n);
-    printArray(A, n);
+    int n;
+    int arr[] = {5, 4, 3, 2, 1, 0};
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = i + 1; j < 6; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
-// #include <stdio.h>
-// #include <stdlib.h>
+// Input Array (There will be total n-1 passes. 5-1 = 4 in this case!)
+//  00  01  02  03  04
+// |03, 05, 02, 13, 12
 
-// int main()
-// {
-//     int n;
-//     int arr[] = {5, 4, 3, 2, 1, 0};
-//     for (int i = 0; i < 6; i++)
-//     {
-//         for (int j = i + 1; j < 6; j++)
-//         {
-//             if (arr[i] > arr[j])
-//             {
-//                 int temp = arr[i];
-//                 arr[i] = arr[j];
-//                 arr[j] = temp;
-//             }
-//         }
-//     }
-//     for (int i = 0; i < 6; i++)
-//     {
-//         printf("%d ", arr[i]);
-//     }
+// After first pass
+//  00  01  02  03  04
+//  02,|05, 03, 13, 12
 
-//     return 0;
-// }
+// After second pass
+// 00  01  02  03  04
+// 02, 03,|05, 13, 12
+
+// After third pass
+// 00  01  02  03  04
+// 02, 03, 05,|13, 12
+
+// After fourth pass
+// 00  01  02  03  04
+// 02, 03, 05, 12,|13
+
+// 5 4 3 2 1
+// 4 5 3 2 1
+// 3 5 4 2 1
+// 2 5 4 3 1
+// 1 5 4 3 2
