c6a6023d8916cceb64edfc6ce431a8fe8497ed00
Pawan <pawankumar08892@gmail.com>
2022-10-22 20:40:37 +0530

    addint more

--- Array/Unique.cpp
+++ Array/Unique.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main()
 {
     int n;
-    int arr[n];
     cin >> n;
+    int arr[n];
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
@@ -17,6 +17,17 @@ int main()
     {
         ans = ans ^ arr[i];
     }
-
+    cout << ans;
     return 0;
 }
+// 00
+// 01
+// --
+// 01
+// 10
+// 11
+// 10
+// 01
+// 10
+// 11
+// I think this only work for when only to duplicate element persent in array 
